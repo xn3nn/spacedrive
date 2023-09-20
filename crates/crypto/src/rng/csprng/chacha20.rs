615c471a4054b9bdce14871a4ad7e235f7108814
brxken128 <77554505+brxken128@users.noreply.github.com>
2023-09-20 22:39:57 +0100

    feat(crypto): massive RNG upgrade. use secure enclave if available, or `ChaCha20Rng` otherwise

--- crates/crypto/src/rng/csprng/chacha20.rs
+++ crates/crypto/src/rng/csprng/chacha20.rs
@@ -1,3 +1,4 @@
+use super::STATE_WORDS;
 use rand::RngCore;
 use rand_chacha::ChaCha20Rng;
 use rand_core::{block::BlockRngCore, SeedableRng};
@@ -9,49 +10,12 @@ use zeroize::{Zeroize, Zeroizing};
 /// values unpredictable.
 pub struct CryptoRng(Box<ChaCha20Rng>);
 
-const STATE_WORDS: usize = 16;
-
-impl rand::CryptoRng for CryptoRng {}
-
-impl RngCore for CryptoRng {
-	#[inline]
-	fn fill_bytes(&mut self, dest: &mut [u8]) {
-		self.fill_bytes(dest);
-		panic!("chacha");
-	}
-
-	#[inline]
-	fn next_u32(&mut self) -> u32 {
-		self.next_u32()
-	}
-
-	#[inline]
-	fn next_u64(&mut self) -> u64 {
-		self.next_u64()
-	}
-
-	#[inline]
-	fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), rand::Error> {
-		self.try_fill_bytes(dest)
-	}
-}
-
-impl BlockRngCore for CryptoRng {
-	type Item = u32;
-	type Results = [u32; STATE_WORDS];
-
-	#[inline]
-	fn generate(&mut self, results: &mut Self::Results) {
-		(0..STATE_WORDS).for_each(|i| results[i] = self.next_u32());
-	}
-}
-
 impl CryptoRng {
 	/// This creates a new `ChaCha20Rng`-backed `CryptoRng` from entropy (via the `getrandom` crate).
 	#[inline]
 	#[must_use]
 	pub fn new() -> Self {
-		Self(ChaCha20Rng::from_entropy().into())
+		Self(Box::new(ChaCha20Rng::from_entropy()))
 	}
 
 	/// Used to generate completely random bytes, with the use of `ChaCha20`
@@ -75,16 +39,41 @@ impl CryptoRng {
 	}
 }
 
+impl RngCore for CryptoRng {
+	#[inline]
+	fn fill_bytes(&mut self, dest: &mut [u8]) {
+		self.0.fill_bytes(dest);
+	}
+
+	#[inline]
+	fn next_u32(&mut self) -> u32 {
+		self.0.next_u32()
+	}
+
+	#[inline]
+	fn next_u64(&mut self) -> u64 {
+		self.0.next_u64()
+	}
+
+	#[inline]
+	fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), rand::Error> {
+		self.0.try_fill_bytes(dest)
+	}
+}
+
+impl BlockRngCore for CryptoRng {
+	type Item = u32;
+	type Results = [u32; STATE_WORDS];
+
+	#[inline]
+	fn generate(&mut self, results: &mut Self::Results) {
+		(0..STATE_WORDS).for_each(|i| results[i] = self.next_u32());
+	}
+}
+
 impl Zeroize for CryptoRng {
 	#[inline]
 	fn zeroize(&mut self) {
 		*self.0 = ChaCha20Rng::from_entropy();
-	}
-}
-
-impl Drop for CryptoRng {
-	#[inline]
-	fn drop(&mut self) {
-		self.zeroize();
 	}
 }
--- crates/crypto/src/rng/csprng/mod.rs
+++ crates/crypto/src/rng/csprng/mod.rs
@@ -1,11 +1,27 @@
+use zeroize::Zeroize;
+const STATE_WORDS: usize = 16;
+
 #[cfg(all(any(target_os = "macos", target_os = "ios"), feature = "hw-rng"))]
 mod apple;
-
 #[cfg(all(any(target_os = "macos", target_os = "ios"), feature = "hw-rng"))]
 pub use apple::CryptoRng;
 
 #[cfg(not(any(feature = "hw-rng", not(any(target_os = "macos", target_os = "ios")))))]
 mod chacha20;
-
 #[cfg(not(any(feature = "hw-rng", not(any(target_os = "macos", target_os = "ios")))))]
 pub use chacha20::CryptoRng;
+
+impl rand::CryptoRng for CryptoRng {}
+
+impl Default for CryptoRng {
+	fn default() -> Self {
+		Self::new()
+	}
+}
+
+impl Drop for CryptoRng {
+	#[inline]
+	fn drop(&mut self) {
+		self.zeroize();
+	}
+}
