9e2e1f457a80051f3079d189b03f8caca76d9b51
Brendan Allan <brendonovich@outlook.com>
2023-10-14 01:15:37 +0800

    move rustc-link-search to build script

--- core/build.rs
+++ core/build.rs
@@ -8,4 +8,15 @@ fn main() {
 	let git_hash = String::from_utf8(output.stdout)
 		.expect("Error passing output of `git rev-parse --short HEAD`");
 	println!("cargo:rustc-env=GIT_HASH={git_hash}");
+
+	#[cfg(any(target_os = "macos", target_os = "windows", target_os = "linux"))]
+	println!(
+		"cargo:rustc-link-search={}",
+		std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+			.join("../apps/.deps/lib")
+			.display()
+	);
+
+	#[cfg(target_os = "linux")]
+	println!("cargo:rustc-link-arg=-Wl,-rpath=${ORIGIN}/../lib/spacedrive");
 }
