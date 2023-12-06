5a960d740771250de9b63c9b951b01b8bf746b09
Utku Bakir <74243531+utkubakir@users.noreply.github.com>
2023-12-06 15:17:27 -0500

    some styling and changes

--- interface/app/$libraryId/Layout/CMDK.tsx
+++ interface/app/$libraryId/Layout/CMDK.tsx
@@ -1,87 +1,112 @@
 import './CMDK.css';
+import './CMDK.scss';
 
+import clsx from 'clsx';
 import { useState } from 'react';
-import CommandPalette, { filterItems, getItemIndex } from 'react-cmdk';
+import CommandPalette, { filterItems, getItemIndex, JsonStructure } from 'react-cmdk';
+import { arraysEqual, useLibraryQuery, useOnlineLocations } from '@sd/client';
+import { CheckBox } from '@sd/ui';
+import { Icon } from '~/components';
 
 const CMDK = (props: { open: boolean; setOpen: (open: boolean) => void }) => {
-	const [page, setPage] = useState<'root' | 'projects'>('root');
+	const [page, setPage] = useState<'root' | 'actions'>('root');
 	const [search, setSearch] = useState('');
+
+	const locationsQuery = useLibraryQuery(['locations.list'], { keepPreviousData: true });
+	const onlineLocations = useOnlineLocations();
+
+	function handleClose(open: boolean) {
+		props.setOpen(open);
+		// Reset page after closing
+		setPage('root');
+	}
 
 	const filteredItems = filterItems(
 		[
 			{
-				heading: 'Home',
-				id: 'home',
-				items: [
-					{
-						id: 'home',
-						children: 'Home',
-						icon: 'HomeIcon',
-						href: '#'
-					},
-					{
-						id: 'settings',
-						children: 'Settings',
-						icon: 'CogIcon',
-						href: '#'
-					},
-					{
-						id: 'projects',
-						children: 'Projects',
-						icon: 'RectangleStackIcon',
-						closeOnSelect: false,
-						onClick: () => {
-							setPage('projects');
-						}
-					}
-				]
-			},
-			{
-				heading: 'Other',
-				id: 'advanced',
-				items: [
-					{
-						id: 'developer-settings',
-						children: 'Developer settings',
-						icon: 'CodeBracketIcon',
-						href: '#'
-					},
-					{
-						id: 'privacy-policy',
-						children: 'Privacy policy',
-						icon: 'LifebuoyIcon',
-						href: '#'
-					},
-					{
-						id: 'log-out',
-						children: 'Log out',
-						icon: 'ArrowRightOnRectangleIcon',
-						onClick: () => {
-							alert('Logging out...');
-						}
-					}
-				]
+				heading: 'Locations',
+				id: 'locations',
+				items: locationsQuery.data
+					? locationsQuery.data.map((location) => ({
+							id: location.id,
+							children: location.name,
+							icon: () => (
+								<div className="relative -mt-0.5 mr-1 shrink-0 grow-0">
+									<Icon name="Folder" size={22} />
+									<div
+										className={clsx(
+											'absolute bottom-0.5 right-0 h-1.5 w-1.5 rounded-full',
+											onlineLocations.some((l) =>
+												arraysEqual(location.pub_id, l)
+											)
+												? 'bg-green-500'
+												: 'bg-red-500'
+										)}
+									/>
+								</div>
+							),
+							href: `#`
+					  }))
+					: ([] as any)
 			}
+			// {
+			// 	heading: 'Actions',
+			// 	id: 'actions',
+			// 	items: [
+			// 		{
+			// 			id: 'new-folder',
+			// 			children: 'New folder',
+			// 			icon: 'FolderPlusIcon',
+			// 			onClick: () => {}
+			// 		},
+			// 		{
+			// 			id: 'new-tag',
+			// 			children: 'New tag',
+			// 			icon: 'TagIcon',
+			// 			onClick: () => {}
+			// 		}
+			// 	]
+			// }
 		],
 		search
 	);
 
+	const paletteItems: JsonStructure = [
+		// These items will always be shown on top of the list
+		{
+			// heading: 'Home',
+			id: 'top',
+			items: [
+				{
+					id: 'ask-anything',
+					children: 'Ask Anything',
+					icon: 'StarIcon',
+					closeOnSelect: false,
+					onClick: () => setPage('actions')
+				}
+			]
+		},
+		...filteredItems
+	];
+
 	return (
 		<CommandPalette
 			onChangeSearch={setSearch}
-			onChangeOpen={props.setOpen}
+			onChangeOpen={handleClose}
 			search={search}
 			isOpen={props.open}
 			page={page}
+			placeholder="Search for files and actions..."
+			// footer
 		>
 			<CommandPalette.Page id="root">
-				{filteredItems.length ? (
-					filteredItems.map((list) => (
+				{paletteItems.length ? (
+					paletteItems.map((list) => (
 						<CommandPalette.List key={list.id} heading={list.heading}>
 							{list.items.map(({ id, ...rest }) => (
 								<CommandPalette.ListItem
 									key={id}
-									index={getItemIndex(filteredItems, id)}
+									index={getItemIndex(paletteItems, id)}
 									{...rest}
 								/>
 							))}
@@ -92,7 +117,28 @@ const CMDK = (props: { open: boolean; setOpen: (open: boolean) => void }) => {
 				)}
 			</CommandPalette.Page>
 
-			<CommandPalette.Page id="projects">{/* Projects page */}</CommandPalette.Page>
+			<CommandPalette.Page id="actions">
+				<CommandPalette.List>
+					<div className="space-y-4 p-4">
+						<div className="flex items-center space-x-2 pt-2">
+							<CheckBox className="!mt-0" />
+							<p className="text-sm text-ink">Enable Action A</p>
+						</div>
+						<div className="flex items-center space-x-2 pt-2">
+							<CheckBox className="!mt-0" />
+							<p className="text-sm text-ink">Enable Action B</p>
+						</div>
+						<div className="flex items-center space-x-2 pt-2">
+							<CheckBox className="!mt-0" />
+							<p className="text-sm text-ink">Enable Action C</p>
+						</div>
+						<div className="flex items-center space-x-2 pt-2">
+							<CheckBox className="!mt-0" />
+							<p className="text-sm text-ink">Enable Action D</p>
+						</div>
+					</div>
+				</CommandPalette.List>
+			</CommandPalette.Page>
 		</CommandPalette>
 	);
 };
